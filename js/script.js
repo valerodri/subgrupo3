5788f85e2ac9d475f6ea4dd372c8581cc4dadfee
franciscobournissen <franciscobournissen@gmail.com>
2023-08-08 11:33:13 -0300

    Soluición a la carga de nombres de los estudiantes

--- js/script.js
+++ js/script.js
@@ -10,12 +10,49 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
-function showData(dataArray) {
-  // El for itera sobre los elementos del array
-  for (const item of dataArray) {
-    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
-  }
-}
+
+let namesArray = [];
+//let studentsNames = JSON.stringify(namesArray);
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+function getJSONData(url) {
+    let result = {};
+    //showSpinner();
+    return fetch(url) //fetchh
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(function (response) {
+            result.status = "ok";
+            result.data = response;
+            //hideSpinner();
+            return result;
+        })
+        .catch(function (error) {
+            result.status = "error";
+            result.data = error;
+            //hideSpinner();
+            return result;
+        });
+}
+
+function showData(dataArray) {
+    // El for itera sobre los elementos del array
+    for (const item of dataArray.students) {
+        // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
+        container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(DATA_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            namesArray = resultObj.data;
+            showData(namesArray);
+        }
+    });
+});
